90591499a1291511372a57d9e10ac7cec61c185e
HendIbrahim2000 <hendibrahim2000@gmail.com>
2022-06-29 08:46:52 +0200

    create

---
+++ DapperDemo/Repository/CompanyRepositorySP.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using DapperDemo.Data;
+using DapperDemo.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositorySP : ICompanyRepository
+    {
+        private IDbConnection db;
+        public CompanyRepositorySP(IConfiguration configuration)
+        {
+            this.db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+        public Company Add(Company company)
+        {
+            var paramters = new DynamicParameters();
+            paramters.Add("@CompanyId", 0, DbType.Int32, direction: ParameterDirection.Output);
+            paramters.Add("@Name", company.Name);
+            paramters.Add("@Address", company.Address);
+            paramters.Add("@City", company.City);
+            paramters.Add("@State", company.State);
+            paramters.Add("@PostalCode", company.PostalCode);
+
+            this.db.Execute("usp_AddCompany", paramters, commandType: CommandType.StoredProcedure);
+            company.CompanyId = paramters.Get<int>("@CompanyId");
+
+            return company;
+        }
+
+        public Company Find(int id)
+        {
+            return db.Query<Company>("usp_GetCompany",new { CompanyId = id}, commandType: CommandType.StoredProcedure).SingleOrDefault();
+        }
+
+        public List<Company> GetAll()
+        {
+            return db.Query<Company>("usp_GetALLCompany", commandType: CommandType.StoredProcedure).ToList();
+        }
+
+        public void Remove(int id)
+        {
+             db.Execute("usp_RemoveCompany", new { CompanyId = id }, commandType: CommandType.StoredProcedure);
+        }
+
+        public Company Update(Company company)
+        {
+            var paramters = new DynamicParameters();
+            paramters.Add("@CompanyId", company.CompanyId, DbType.Int32);
+            paramters.Add("@Name", company.Name);
+            paramters.Add("@Address", company.Address);
+            paramters.Add("@City", company.City);
+            paramters.Add("@State", company.State);
+            paramters.Add("@PostalCode", company.PostalCode);
+
+            this.db.Execute("usp_UpdateCompany", paramters, commandType: CommandType.StoredProcedure);
+
+            return company;
+        }
+    }
+}
---
+++ DapperDemo/Repository/IBonusRepository.cs
@@ -0,0 +1,14 @@
+﻿using DapperDemo.Models;
+
+namespace DapperDemo.Repository
+{
+    public interface IBonusRepository
+    {
+        List<Employee> GetEmployeeWithCompany(int id);
+        Company GetCompanyWithEmployees(int id);
+        List<Company> GetAllCompanyWithEmployees();
+        void AddTestCompanyWithEmployee(Company objcomp);
+        void RemoveRange(int[] companyId);
+        List<Company> FilterCompanyByName(string name);
+    }
+}
